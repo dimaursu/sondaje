6dbb612446b4bee7188ea8695f3d8cdc0c7f6e2b
Dumitru Ursu <dima@ceata.org>
2013-02-27 17:42:52 +0200

    Initial commit

---
+++ app/assets/javascripts/votare.js.coffee
@@ -0,0 +1,9 @@
+window.Votare =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Votare.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votare::Application.config.secret_token = '8594e8847303280c751f1d0b9d11cf51bc04565ec6c59b0119333bb11b515050dca5bb4e0da24dde4b04ea03325047116055c7d2a52915b79d49bb428f1da2f0'
